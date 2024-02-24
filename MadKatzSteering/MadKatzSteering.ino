d5ed30a168f670ae7056f3eee81c7d95a662f105
Lightbender255 <lightbender255@gmail.com>
2024-02-24 03:12:07 -0600

    Everything seems to be working.  Steering is a bit janky going left

--- MadKatzSteering/MadKatzSteering.ino
+++ MadKatzSteering/MadKatzSteering.ino
@@ -1,11 +1,36 @@
 #include <Joystick.h>
-#include <USBAPI.h>
+
+const bool INCLUDE_X_AXIS = true;
+const bool INCLUDE_Y_AXIS = true;
+const bool INCLUDE_Z_AXIS = true;
+const bool INCLUDE_RX_AXIS = true;
+const bool INCLUDE_RY_AXIS = true;
+const bool INCLUDE_RZ_AXIS = true;
+const bool INCLUDE_RUDDER = true;
+const bool INCLUDE_THROTTLE = true;
+const bool INCLUDE_ACCELERATOR = true;
+const bool INCLUDE_BRAKE = true;
+const bool INCLUDE_STEERING = true;
+
+const int BRAKE_ADJUSTMENT = 17;
+const int GAS_ADJUSTMENT = 33;
+const int STEERING_ADJUSTMENT = 0;
+const int BUTTON_COUNT = 4;
+const int HAT_SWITCH_COUNT = 0;
+const int GAS_MIN = 400;
+const int BRAKE_MIN = 0;
+const int STEERING_MIN = 0;
+const int GAS_MAX = 1023;
+
+const int OUTPUT_SERIAL_TEXT = false;
+const int OUTPUT_SERIAL_GRAPH = true;
 
 // Create Joystick
-Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
-                   2, 0,
-                   true, true, true, true, true, true,
-                   true, true, true, true, true);
+Joystick_ Joystick(
+    JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_JOYSTICK,
+    2, 0,
+    true, true, true, true, true, true,
+    true, true, true, true, true);
 
 int hysteresis(int newValue, int oldValue)
 {
@@ -44,43 +69,70 @@ const int pinToButtonMap = 9;
 // Last state of the button
 int lastButtonState[4] = {0, 0, 0, 0};
 
-int oldSteeringValue = 0;
-int oldGasValue = 0;
-int oldBrakeValue = 0;
+int previousSteeringValue = 0;
+int previousGasValue = 0;
+int previousBrakeValue = 0;
+int brake = 0;
+int gas = 0;
+int steering = 0;
 
 void loop()
 {
-  int steering = analogRead(A0);
-  steering = hysteresis(steering, oldSteeringValue);
+  steering = analogRead(A0);
+  steering = hysteresis(steering, previousSteeringValue);
+  steering -= STEERING_ADJUSTMENT;
   Joystick.setXAxis(steering);
-  Joystick.setSteering(steering);
 
-  int brake = analogRead(A2);
-  brake = hysteresis(brake, oldGasValue);
-  Joystick.setRxAxis(brake);
-
-  int gas = analogRead(A1);
-  gas = hysteresis(gas, oldGasValue);
+  gas = analogRead(A1);
+  gas = hysteresis(gas, previousGasValue);
+  gas = gas - GAS_ADJUSTMENT;
+  gas = GAS_MAX - gas;
+  if (gas < GAS_MIN)
+  {
+    gas = 0;
+  }
   Joystick.setRyAxis(gas);
 
-  Serial.print("\tSteering X: [ ");
-  Serial.print(steering);
-  Serial.print("% ]");
+  brake = analogRead(A2);
+  brake = hysteresis(brake, previousBrakeValue);
+  brake = brake - BRAKE_ADJUSTMENT;
+  if (brake < 0)
+  {
+    brake = 0;
+  }
+  Joystick.setRxAxis(brake);
 
-  Serial.print("\tSteering: [ ");
-  Serial.print(steering);
-  Serial.print("% ]");
+  if (OUTPUT_SERIAL_TEXT)
+  {
 
-  Serial.print("\tGas: [ ");
-  Serial.print(gas);
-  Serial.print("% ]");
+    Serial.print("\tSteering X: [ ");
+    Serial.print(steering);
+    Serial.print(" ]");
 
-  Serial.print("\tBrake: [ ");
-  Serial.print(brake);
-  Serial.print("% ]");
+    Serial.print("\tSteering: [ ");
+    Serial.print(steering);
+    Serial.print(" ]");
 
-  Serial.println();
-  delay(100);
+    Serial.print("\tGas: [ ");
+    Serial.print(gas);
+    Serial.print(" ]");
+
+    Serial.print("\tBrake: [ ");
+    Serial.print(brake);
+    Serial.print(" ]");
+
+    Serial.println();
+    delay(100);
+  }
+
+  if (OUTPUT_SERIAL_GRAPH)
+  {
+    Serial.print(steering);
+    Serial.print(",");
+    Serial.print(gas);
+    Serial.print(",");
+    Serial.print(brake);
+  }
 
   for (int index = 0; index < 4; index++)
   {
@@ -90,10 +142,21 @@ void loop()
       Joystick.setButton(index, currentButtonState);
       lastButtonState[index] = currentButtonState;
     }
-    Serial.print("\tButton: [ ");
-    Serial.print(lastButtonState[index]);
-    Serial.print(" ]");
-  }
 
-  delay(50);
+    if (OUTPUT_SERIAL_TEXT)
+    {
+      Serial.print("\tButton: [ ");
+      Serial.print(lastButtonState[index]);
+      Serial.print(" ]");
+    }
+
+    if (OUTPUT_SERIAL_GRAPH)
+    {
+      Serial.print(",");
+      Serial.print(lastButtonState[index]);
+    }
+
+    delay(50);
+  }
+  Serial.println();
 }
